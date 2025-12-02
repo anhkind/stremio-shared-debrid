0be10b26c3b2f909751bdc46163d2b7c7d745b69
Anh Nguyen <anhkind@gmail.com>
2025-12-02 14:57:43 +0800

    Update manifest.js file

--- manifest.js
+++ manifest.js
@@ -1,10 +1,11 @@
 const manifest = {
-  "id":           "com.github.anhkind",
-  "version":      "1.0.0",
-  "name":         "AAA Stremio addon",
-  "description":  "Shared user notifier for Stremio stream",
-  "resources":    ["stream" ],
-  "types":        ["movie", "series", "channel", "tv"],
+  id:           "com.github.anhkind",
+  version:      "1.0.0",
+  name:         "AAA Stremio addon",
+  description:  "Shared user notifier for Stremio streams",
+  resources:    ["stream" ],
+  catalogs:     [],
+  types:        ["movie", "series", "channel", "tv"],
 };
 
 module.exports = manifest;
